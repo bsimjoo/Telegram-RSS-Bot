332866ed1cc4ecf7c06350a2547680c5076f3fff
bsimjoo <bsimjoo@gmail.com>
2021-04-21 11:28:41 +0430

    updated

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,23 +1,13 @@
-function downloadURI(uri, name) 
-{
-    var link = document.createElement("a");
-    // If you don't know the name or want to use
-    // the webserver default set name = ''
-    link.setAttribute('download', name);
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-}
+window.onload = function ()  {
+    let loading = document.getElementById("loading-screen");
+    this.sleep(2000);
+    loading.style.display = "none";
+  };
 
-function downloadLatest() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var myObj = JSON.parse(this.responseText);
-            downloadURI(myObj.zipball_url,'')
-        }
-    };
-    xmlhttp.open("GET", "https://api.github.com/repos/bsimjoo/Telegram-RSS-Bot/releases/latest", true);
-    xmlhttp.send();
-}
+  function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
